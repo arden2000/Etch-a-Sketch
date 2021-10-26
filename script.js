1c5d1b24d43de90b7b074b089edfc9d79131bb70
Arden <ardenozdere@gmail.com>
2021-10-26 10:50:23 -0700

    lighten/darken feature

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const START_GRID_SIZE = 16;
 let mouseDown = false;
 let mode = 'color';
 
+// https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
 const showGridBtn = document.querySelector('button#show-grid');
 showGridBtn.addEventListener('click', () => {
     showGridBtn.classList.toggle('active');
@@ -21,8 +24,6 @@ gridContainer.addEventListener('mouseleave', () => {
     mouseDown = false;
 }, { capture: false });
 
-updateGrid(START_GRID_SIZE);
-
 const gridSlider = document.querySelector('input#grid-size-slider');
 const gridSizeOutput = document.querySelector('span#slider-output');
 gridSlider.min = 16;
@@ -37,26 +38,28 @@ gridSlider.addEventListener("mousemove", function (e) {
 });
 
 const eraserBtn = document.querySelector('button#eraser');
-eraserBtn.addEventListener("click", toggleEraser);
+eraserBtn.addEventListener('click', () => {toggleMode('eraser', eraserBtn);});
 
 const rainbowBtn = document.querySelector('button#rainbow');
-rainbowBtn.addEventListener("click",toggleRainbow);
+rainbowBtn.addEventListener('click', () => {toggleMode('rainbow', rainbowBtn);});
 
 const clearBtn = document.querySelector('button#clear');
 clearBtn.addEventListener('click', clearGrid);
 
-function toggleEraser(){
-    mode = (mode == 'eraser') ? 'color' : 'eraser';
-    (mode == 'eraser') ? eraserBtn.classList.add('active') 
-        : eraserBtn.classList.remove('active');
-    rainbowBtn.classList.remove('active');
-}
+const lightenBtn = document.querySelector('button#lighten');
+lightenBtn.addEventListener('click', () => {toggleMode('lighten', lightenBtn);});
 
-function toggleRainbow(){
-    mode = (mode == 'rainbow') ? 'color' : 'rainbow';
-    (mode == 'rainbow') ? rainbowBtn.classList.add('active') 
-        : rainbowBtn.classList.remove('active');
-    eraserBtn.classList.remove('active');
+const darkenBtn = document.querySelector('button#darken');
+darkenBtn.addEventListener('click', () => {toggleMode('darken', darkenBtn);});
+
+updateGrid(START_GRID_SIZE);
+
+function toggleMode(modeStr, btn){
+    if (btn == undefined) return;
+    turnOffOtherButtons(btn);
+    mode = (mode == modeStr) ? 'color' : modeStr;
+    (mode == modeStr) ? btn.classList.add('active') 
+        : btn.classList.remove('active');
 }
 
 function clearGrid() {
@@ -104,7 +107,20 @@ function colorCell(cell) {
         case 'color':
             cell.style.backgroundColor = colorPicker.value;
             break;
+        case 'lighten':
+            cell.style.backgroundColor = LightenDarkenColor(rgb2hex(cell.style.backgroundColor), 20);
+            break;
+        case 'darken':
+            cell.style.backgroundColor = LightenDarkenColor(rgb2hex(cell.style.backgroundColor), -20);
+            break;
     }
+}
+
+function turnOffOtherButtons(btn){
+    if (btn != eraserBtn) eraserBtn.classList.remove('active');
+    if (btn != rainbowBtn) rainbowBtn.classList.remove('active');
+    if (btn != darkenBtn) darkenBtn.classList.remove('active');
+    if (btn != lightenBtn) lightenBtn.classList.remove('active');
 }
 
 function updateGrid(gridSize) {
@@ -126,4 +142,32 @@ function updateGrid(gridSize) {
     if (showGridBtn.classList.contains('active')){
         toggleShowGrid();
     }
+}
+
+//https://css-tricks.com/snippets/javascript/lighten-darken-color/
+function LightenDarkenColor(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
 }
