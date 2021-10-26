9db92287c7dd12fdddef0cb2c19f616e9d3c4645
Arden <ardenozdere@gmail.com>
2021-10-26 01:00:50 -0700

    additional features and ui

--- script.js
+++ script.js
@@ -1,17 +1,111 @@
-const gridContainer = document.querySelector('div.grid-container');
-const colorPicker = document.querySelector('input#colorpicker');
-let mouseDown = false;
-let eraserOn = false;
-let rainbowOn = false;
+const START_GRID_SIZE = 16;
 
-colorPicker.addEventListener('change', () => {
-    turnEraserOff();
-    turnRainbowOff();
+let mouseDown = false;
+let mode = 'color';
+
+const showGridBtn = document.querySelector('button#show-grid');
+showGridBtn.addEventListener('click', () => {
+    showGridBtn.classList.toggle('active');
+    toggleShowGrid();
 });
 
+const colorPicker = document.querySelector('input#colorpicker');
+colorPicker.addEventListener('change', () => {
+    mode = 'color';
+    eraserBtn.classList.remove('active');
+    rainbowBtn.classList.remove('active');
+});
+
+const gridContainer = document.querySelector('div.grid-container');
 gridContainer.addEventListener('mouseleave', () => {
     mouseDown = false;
-}, {capture: false});
+}, { capture: false });
+
+updateGrid(START_GRID_SIZE);
+
+const gridSlider = document.querySelector('input#grid-size-slider');
+const gridSizeOutput = document.querySelector('span#slider-output');
+gridSlider.min = 16;
+gridSlider.max = 64;
+gridSlider.value = 16;
+gridSizeOutput.textContent = gridSlider.value + ' x ' + gridSlider.value;
+gridSlider.addEventListener("change", function (e) {
+    updateGrid(gridSlider.value);
+});
+gridSlider.addEventListener("mousemove", function (e) {
+    gridSizeOutput.textContent = gridSlider.value + ' x ' + gridSlider.value;
+});
+
+const eraserBtn = document.querySelector('button#eraser');
+eraserBtn.addEventListener("click", toggleEraser);
+
+const rainbowBtn = document.querySelector('button#rainbow');
+rainbowBtn.addEventListener("click",toggleRainbow);
+
+const clearBtn = document.querySelector('button#clear');
+clearBtn.addEventListener('click', clearGrid);
+
+function toggleEraser(){
+    mode = (mode == 'eraser') ? 'color' : 'eraser';
+    (mode == 'eraser') ? eraserBtn.classList.add('active') 
+        : eraserBtn.classList.remove('active');
+    rainbowBtn.classList.remove('active');
+}
+
+function toggleRainbow(){
+    mode = (mode == 'rainbow') ? 'color' : 'rainbow';
+    (mode == 'rainbow') ? rainbowBtn.classList.add('active') 
+        : rainbowBtn.classList.remove('active');
+    eraserBtn.classList.remove('active');
+}
+
+function clearGrid() {
+    (gridContainer.childNodes).forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+}
+
+function toggleShowGrid(){
+    (gridContainer.childNodes).forEach(cell => {
+        cell.classList.toggle('visible');
+    });
+}
+
+function createGridSquare() {
+    let grid = document.createElement('div');
+    grid.style.backgroundColor = 'white';
+    grid.classList.add('cell');
+
+    grid.addEventListener('mouseover', function (e) {
+        if (mouseDown) {
+            colorCell(this);
+        }
+    });
+    grid.addEventListener('mousedown', function (e) {
+        mouseDown = true;
+        colorCell(this);
+    });
+
+    grid.addEventListener('mouseup', function (e) {
+        mouseDown = false;
+    });
+    return grid;
+}
+
+function colorCell(cell) {
+    switch (mode) {
+        case 'rainbow':
+            // https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+            cell.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+            break;
+        case 'eraser':
+            cell.style.backgroundColor = 'white';
+            break;
+        case 'color':
+            cell.style.backgroundColor = colorPicker.value;
+            break;
+    }
+}
 
 function updateGrid(gridSize) {
     // clears grid
@@ -22,123 +116,14 @@ function updateGrid(gridSize) {
     //updates grid style attributes to fit new size
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
+    
     // fills grid
     for (let i = 0; i < gridSize ** 2; i++) {
         gridContainer.appendChild(createGridSquare());
     }
-}
 
-updateGrid(16);
-
-const gridSlider = document.querySelector('input#grid-size-slider');
-const gridSizeOutput = document.querySelector('span#slider-output');
-gridSlider.min = 16;
-gridSlider.max = 64;
-gridSlider.value = 16;
-
-gridSizeOutput.textContent = gridSlider.value + ' x ' + gridSlider.value;
-gridSlider.addEventListener("change", function (e) {
-    updateGrid(gridSlider.value);
-});
-gridSlider.addEventListener("mousemove", function (e) {
-    gridSizeOutput.textContent = gridSlider.value + ' x ' + gridSlider.value;
-});
-
-const eraserBtn = document.querySelector('button#eraser');
-eraserBtn.addEventListener("click", () => {
-    turnRainbowOff();
-    eraserOn ? turnEraserOff() : turnEraserOn();
-});
-
-function turnEraserOn(){
-    eraserOn = true;
-    eraserBtn.style.backgroundColor = 'red';
-}
-
-function turnEraserOff(){
-    eraserOn = false;
-    eraserBtn.style.backgroundColor = 'white';
-}
-
-/*function toggleEraser() {
-    if (rainbowOn) toggleRainbow();
-    eraserBtn.style.backgroundColor = eraserBtn.style.backgroundColor == 'white' ? 'red' : 'white';
-    eraserOn = !eraserOn;
-}*/
-
-const rainbowBtn = document.querySelector('button#rainbow');
-rainbowBtn.addEventListener("click", () => {
-    turnEraserOff();
-    rainbowOn ? turnRainbowOff() : turnRainbowOn();
-});
-
-/*function toggleRainbow() {
-    if (eraserOn) toggleEraser(eraserBtn);
-    rainbowBtn.style.backgroundColor = rainbowBtn.style.backgroundColor == 'white' ? 'green' : 'white';
-    rainbowOn = !rainbowOn;
-}*/
-
-function turnRainbowOn(){
-    rainbowOn = true;
-    rainbowBtn.style.backgroundColor = 'blue';
-}
-
-function turnRainbowOff(){
-    rainbowOn = false;
-    rainbowBtn.style.backgroundColor = 'white';
-}
-
-function createGridSquare() {
-    let grid = document.createElement('div');
-    grid.style.backgroundColor = 'white';
-    
-    grid.addEventListener('mouseover', function (e) {
-        if (mouseDown) {
-            if (rainbowOn) {
-                // https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
-                this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-                this.classList.add('changed');
-            }
-            else if (eraserOn) {
-                this.style.backgroundColor = 'white';
-                this.classList.remove('changed');
-            }
-            else {
-                this.style.backgroundColor = colorPicker.value;
-                this.classList.add('changed');
-            }
-        }
-        else if (!this.classList.contains('changed')) {
-            this.style.backgroundColor = 'grey';
-        }
-    });
-    
-    grid.addEventListener('mouseout', function (e) {
-        if (!this.classList.contains('changed')) {
-            this.style.backgroundColor = 'white';
-        }
-    });
-
-    grid.addEventListener('mousedown', function (e) {
-        mouseDown = true;
-        if (rainbowOn) {
-            this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-            this.classList.add('changed');
-        }
-        else if (eraserOn) {
-            this.style.backgroundColor = 'white';
-            this.classList.remove('changed');
-        }
-        else {
-            this.style.backgroundColor = colorPicker.value;
-            this.classList.add('changed');
-        }
-    });
-
-    grid.addEventListener('mouseup', function (e) {
-        mouseDown = false;
-    });
-
-    return grid;
+    // keeps grid visible if option is selected
+    if (showGridBtn.classList.contains('active')){
+        toggleShowGrid();
+    }
 }
